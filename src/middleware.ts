dc434a70b4f6c8c9e55c83047894ae8727cef9a0
Aldino Kemal <aldinokemal2104@gmail.com>
2025-02-23 23:23:47 +0700

    feat: update middleware matcher to support dynamic paths for booking routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,7 +41,7 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/booking',
+    '/booking/:path*',
     '/my-bookings/:path*',
     '/auth/:path*',
     '/account/:path*',
